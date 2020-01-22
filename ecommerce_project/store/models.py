7711554f2d14f82ca3670aa678fd5d329c8a9368
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-01-22 22:04:12 +0000

    added product view on admin and created Class Order in models.py

--- ecommerce_project/store/models.py
+++ ecommerce_project/store/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse
 # Create your models here.
+
+# Model: Category
 class Category(models.Model):
     name = models.CharField(max_length=250, unique=True)
     slug = models.SlugField(max_length=250, unique=True)
@@ -18,6 +20,8 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
+# Model: Product
 class Product(models.Model):
     name = models.CharField(max_length=250, unique=True)
     slug = models.SlugField(max_length=250, unique=True)
@@ -43,8 +47,6 @@ class Product(models.Model):
 
 
 # Model: Cart
-
-
 class Cart(models.Model):
     cart_id = models.CharField(max_length=250, blank=True)
     date_added = models.DateField(auto_now_add=True)
@@ -67,6 +69,43 @@ class CartItem(models.Model):
 
     def sub_total(self):
         return self.product.price * self.quantity
+
+    def __str__(self):
+        return self.product
+
+
+# Model: Order
+class Order(models.Model):
+    token = models.CharField(max_length=250, unique=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='USD Order Total')
+    emailAdress = models.EmailField(max_length=250, blank=True, verbose_name='Email Adress')
+    created = models.DateTimeField(auto_now_add=True)
+    billingName = models.CharField(max_length=250, blank=True)
+    billingAddress1 = models.CharField(max_length=250, blank=True)
+    billingCity = models.CharField(max_length=250, blank=True)
+    billingPostcode = models.CharField(max_length=250, blank=True)
+    billingCountry = models.CharField(max_length=250, blank=True)
+    shippingName = models.CharField(max_length=250, blank=True)
+    shippingAddress1 = models.CharField(max_length=250, blank=True)
+    shippingCity = models.CharField(max_length=250, blank=True)
+    shippingPostCode= models.CharField(max_length=250, blank=True)
+    shippingCountry = models.CharField(max_length=250, blank=True)
+
+    class Meta:
+        db_table = 'Order'
+        ordering = ['-created']
+
+class OrderItem(models.Model):
+    product = models.CharField(max_length=250)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='USD')
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'OrderItem'
+
+    def sub_total(self):
+        return self.quantity * self.price
 
     def __str__(self):
         return self.product
