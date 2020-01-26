50fbf051e47656706f39fe545197e93a03f3a7ea
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-01-26 23:41:48 +0000

    added signup template

--- ecommerce_project/store/views.py
+++ ecommerce_project/store/views.py
@@ -4,6 +4,8 @@ from .models import Category, Product, Cart, CartItem, Order, OrderItem
 from django.core.exceptions import ObjectDoesNotExist
 import stripe
 from django.conf import settings
+from django.contrib.auth.models import Group, User
+from .forms import SignUpForm
 
 # Create your views here.
 
@@ -160,3 +162,16 @@ def thanks_page(request, order_id):
     if order_id:
         customer_order = get_object_or_404(Order, id=order_id)
     return render(request, 'thankyou.html', {'customer_order': customer_order})
+
+def signupView(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid()
+        form.save()
+        username = form.cleaned_data.get('username')
+        signup_user = User.objects.get(username=username)
+        customer_group = Group.objects.get(name='Customer')
+        customer_group.user_set.add(signup_user)
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
