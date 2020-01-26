4c5526c748a8290e9b238df5df10be2018377791
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-01-26 23:56:46 +0000

    fixed signup conditional

--- ecommerce_project/store/forms.py
+++ ecommerce_project/store/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=100, required=True)
-    last_name = orms.CharField(max_length=100, required=True)
+    last_name = forms.CharField(max_length=100, required=True)
     email = forms.EmailField(max_length=250, help_text='eg. youremail@example.com')
 
     class Meta:
--- ecommerce_project/store/views.py
+++ ecommerce_project/store/views.py
@@ -166,12 +166,12 @@ def thanks_page(request, order_id):
 def signupView(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
-        if form.is_valid()
-        form.save()
-        username = form.cleaned_data.get('username')
-        signup_user = User.objects.get(username=username)
-        customer_group = Group.objects.get(name='Customer')
-        customer_group.user_set.add(signup_user)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            signup_user = User.objects.get(username=username)
+            customer_group = Group.objects.get(name='Customer')
+            customer_group.user_set.add(signup_user)
     else:
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
