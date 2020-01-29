bccd440d9975803e776ba5eef0dff3e888d6f479
Oliveira Prado <eliaspradoprofessional@outlook.com>
2020-01-29 23:59:55 +0000

    added heroku installation files

--- ecommerce_project/ecommerce_project/settings.py
+++ ecommerce_project/ecommerce_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '+52*=m!ls*vf2ctbn0m-1r&4n%4uqp@7mifl6lz7h(ut+xt1+p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
